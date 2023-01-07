d4a839d7570c5e15793647c71bf1bb8e7d83b140
Heetae Cho <pank0415@gmail.com>
2023-01-07 22:29:56 +0900

    .

---
+++ src/org/opentutorials/javatutorials/array/LengthDemo.java
@@ -0,0 +1,20 @@
+package org.opentutorials.javatutorials.array;
+
+public class LengthDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//String[] classGroup = { "최진혁", "최유빈", "한이람", "이고잉" };
+		String[] classGroup = new String[4];
+		classGroup[0] = "최진혁";
+		System.out.println(classGroup.length);
+		classGroup[1] = "최유빈";
+		System.out.println(classGroup.length);
+		classGroup[2] = "한이람";
+		System.out.println(classGroup.length);
+		classGroup[3] = "이고잉";
+		System.out.println(classGroup.length);
+
+	}
+
+}
---
+++ src/org/opentutorials/javatutorials/loop/BreakDemo.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.loop;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0; i<10; i++) {
+			if (i==5) 
+				break; // can skip {} if 1 line 
+			System.out.println("Coding Everybody "+i);
+			}
+		}
+
+	}
