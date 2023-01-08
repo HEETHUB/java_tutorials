abecede687929096d7f382b2ddbb31fee8dd3953
Heetae Cho <pank0415@gmail.com>
2023-01-09 00:39:59 +0900

    .

---
+++ src/org/opentutorials/javatutorials/method/MethodDemo4.java
@@ -0,0 +1,18 @@
+package org.opentutorials.javatutorials.method;
+
+public class MethodDemo4 {
+	public static void numbering(int limit) {
+		int i = 0;
+		while (i < limit) {
+			System.out.println(i);
+			i++;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		numbering(5);
+
+	}
+
+}
